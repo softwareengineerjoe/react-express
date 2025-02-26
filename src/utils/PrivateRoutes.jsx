509f59376d6d39661ad1f6bc6afc609316a0ed36
softwareengineerjoe <maranan.mcjo@gmail.com>
2025-02-26 21:55:09 +0800

    feat(web): add tailwind and react router dom

---
+++ src/utils/PrivateRoutes.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import { Outlet, Navigate } from "react-router-dom";
+
+const PrivateRoutes = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(null);
+
+  useEffect(() => {
+    const token = localStorage.getItem("jwtToken");
+    if (token !== null) {
+      setIsAuthenticated(true);
+    } else {
+      setIsAuthenticated(false);
+    }
+  }, []);
+
+  if (isAuthenticated === null) {
+    return <div>Loading...</div>;
+  }
+
+  if (!isAuthenticated) {
+    return <Navigate to="/login" />;
+  }
+
+  return (
+    <div className="relative">
+      <Outlet />
+    </div>
+  );
+};
+
+export default PrivateRoutes;
